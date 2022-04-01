b809e5a46fbb9083792bbaa8bd19b29f10678fe6
Jason Lam <nyjasonlam@gmail.com>
2022-04-01 19:35:52 -0400

    fixed server.js

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+
+
+app.get('*', (req, res) => {
+  res.send("hello world")
+});
+
+app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`);
+});
